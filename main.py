fb9afbdf751f52d9d5f431184531b4c7221430b9
Mazen Mohamed <mazen.business56@gmail.com>
2025-12-16 23:21:07 +0200

    Refactor test_calculate to print calculation results
    Replaced test function with direct calculation and result printing.

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from mean_var_std import calculation
 
-def test_calculate():
-    data = [0,1,2,3,4,5,6,7,8]
-    result = calculation(data)
 
-    # test some elements
-    assert result['Sum'][2] == 36, "Sum of all elements should be 36"
-    assert result['Mean'][2] == 4, "Mean of all elements should be 4.0"
-    assert result['Max'][2] == 8, "Max of all elements should be 8"
-    assert result['Min'][2] == 0, "Min of all elements should be 0"
+result = calculation([1,4,7,8,5,2,3,6,9])
 
-    print("All tests passed!")
-
-
-if __name__ == "__main__":
-    test_calculate()
+# Print the result
+print("== Result ==\n")
+for key in (["Mean", "Variance", "Standard Deviation", "Max", "Min", "Sum"]):
+    print(key.capitalize()), "\n"
+    print(f"Columns: {result[key][0]}")
+    print(f"Rows:    {result[key][1]}")
+    print(f"All:     {result[key][2]}\n")
